bcb3e3a86c478e3b7f3b95f98f744170c9db861f
yirgalemwoldeab <yirga1984@yahoo.com>
2022-08-31 21:41:21 -0500

    hat class created

---
+++ called.java
@@ -0,0 +1,54 @@
+Create a new java project
+Go to VSC -> Enable Version Control
+Create a .gitignore file and add the main files for a java project
+    out, .idea, *.iml  
+Create a new branch: develop
+
+Create a new branch: feature-a
+Checkout to feature-a
+    Create a class called Water
+        add some lines 
+        commit those changes
+    Create a class called Food
+        add some lines 
+        commit those changes
+    Push the changes to your origin feature-a branch
+
+Create a pull request to merge the feature-a changes to the develop branch
+Checkout to local develop and update it
+
+Create a new branch: feature-b
+Checkout to feature-b
+    Create a class called Hat
+        add some lines 
+        commit those changes
+    Update the Hat class
+        add more lines
+        commit those changes
+    Update the Hat class
+        add more lines
+        commit those changes
+    
+    Push the changes to your origin feature-b branch
+
+Create a pull request to merge the feature-b changes to the develop branch
+Checkout to local develop and update it
+
+Create a new branch: feature-c
+Checkout to feature-c
+    Create a class called Store
+        add some lines 
+        commit those changes
+    Update the Store class
+        add more lines
+        commit those changes
+
+    Push the changes to your origin feature-c branch
+
+Create a pull request to merge the feature-c changes to the develop branch
+First version is done, so:
+Create a pull request to merge the develop branch to the master branch
+Checkout to local develop and update it
+Checkout to local master and update it
+
+Repeat these steps to continue practice with the flow by yourselves. 
